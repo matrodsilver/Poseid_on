30e2b6feab4e145ab1978554e28ba678a0b9570c
Gtadashi1001 <142353287+Gtadashi1001@users.noreply.github.com>
2023-08-17 16:30:19 -0300

    Add files via upload
    Adicionando mais páginas(Bem-Vindo e Equipe) e coloquei os cards na parte de consulta do clima.

---
+++ POSEIDON/Bem-Vindo.py
@@ -0,0 +1,6 @@
+import streamlit as sl
+
+sl.title('Seja Bem Vindo ao sistema POSEIDON!')
+sl.header('O que é o POSEIDON?')
+sl.text('O POSEIDON foi criado para fazer com que as pessoas se sintam cada vez mais seguras')
+sl.text('em relação às enchentes que ocorrem em São Paulo.')
