8c2810e198a027cd391abd6c55f69e73ef745af1
MR.S <82432761+matrodsilver@users.noreply.github.com>
2023-08-28 09:48:53 -0300

    Delete POSEIDON directory

--- POSEIDON/Bem-Vindo.py
+++
@@ -1,6 +0,0 @@
-import streamlit as sl
-
-sl.title('Seja Bem Vindo ao sistema POSEIDON!')
-sl.header('O que é o POSEIDON?')
-sl.text('O POSEIDON foi criado para fazer com que as pessoas se sintam cada vez mais seguras')
-sl.text('em relação às enchentes que ocorrem em São Paulo.')
--- POSEIDON/pages/Equipe.py
+++
@@ -1,8 +0,0 @@
-import streamlit as sl
-
-sl.title('Conheça nossa equipe')
-sl.subheader('Gabriel Sakura')
-sl.subheader('Gabriel Tadashi')
-sl.subheader('Gabriel Trusnovec')
-sl.subheader('Giulia Henritzi')
-sl.subheader('Matheus')
