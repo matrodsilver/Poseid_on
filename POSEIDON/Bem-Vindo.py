5f490925fee7227b45846f0132d66c36bee6a478
Gtadashi1001 <142353287+Gtadashi1001@users.noreply.github.com>
2023-08-27 22:29:07 -0300

    Add files via upload
    Arrumei a parte do gráfico e dos valores, além de adicionar informações na parte de "Bem-Vindo" e "Dicas"

--- POSEIDON/Bem-Vindo.py
+++ POSEIDON/Bem-Vindo.py
@@ -1,6 +1,30 @@
 import streamlit as sl
 
-sl.title('Seja Bem Vindo ao sistema POSEIDON!')
-sl.header('O que é o POSEIDON?')
-sl.text('O POSEIDON foi criado para fazer com que as pessoas se sintam cada vez mais seguras')
-sl.text('em relação às enchentes que ocorrem em São Paulo.')
+sl.header('Seja Bem Vindo ao sistema POSEIDON!')
+sl.markdown('''
+    <h3>Sobre o projeto P.O.S.E.I.D.ON</h3>
+                
+    Hoje, trago a vocês uma visão de mudança para as cidades em que vivemos. Comecemos refletindo sobre algo que muitas vezes passa despercebido: os bueiros entupidos. Esses vilões urbanos podem causar estragos significativos, desde enchentes até doenças e caos nas ruas.
+
+    <h3>Desafios dos Bueiros Entupidos</h3>
+                
+    Já se perguntaram sobre os problemas causados por bueiros entupidos? O cenário é sombrio. As chuvas transformam as ruas em rios, causando prejuízos materiais e ameaçando vidas. A água contaminada traz consigo doenças que se espalham rapidamente, afetando a saúde pública. Além disso, o lixo acumulado atrai pragas urbanas, prejudicando a qualidade de vida.
+
+    <h3>Vislumbrando a Mudança</h3>
+                
+    Agora, imagine um futuro em que as enchentes sejam minimizadas, doenças prevenidas e nossas cidades mais limpas e saudáveis. Hoje, apresento a vocês o projeto "Poseidon". Nomeado após o deus grego dos mares, este projeto visa trazer um novo olhar para o gerenciamento de bueiros.
+
+    <h3>O Projeto "Poseidon"</h3>
+                
+    "Possuído" por uma ideia ousada, o projeto "Poseidon" traz soluções tecnológicas de ponta para enfrentar um desafio antigo. Transformaremos os bueiros em ativos inteligentes.
+
+    <h3>Impacto e Futuro</h3>
+                
+    O impacto é notável. Menos enchentes significam cidades mais seguras, economia de recursos e vidas protegidas. Com cidades mais limpas e saudáveis, a qualidade de vida aumenta para todos nós. E o melhor? Este é apenas o começo. Podemos expandir essa solução para outras cidades, em todo o mundo.
+
+    <h3>Conclusão</h3>
+                
+    O projeto "Poseidon" não é apenas uma ideia... é o começo de uma revolução. Ao combinar tecnologia, inovação e colaboração, estamos redefinindo a maneira como nossas cidades enfrentam os desafios das águas das chuvas. Muito obrigado por nos acompanhar hoje. Juntos, estamos construindo cidades mais seguras, limpas e conectadas.
+''', unsafe_allow_html=True)
+
+
