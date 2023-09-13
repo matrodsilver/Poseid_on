66243485737d1afd108959a58523d57491c6e30a
MR.S <82432761+matrodsilver@users.noreply.github.com>
2023-09-13 20:14:07 -0300

    Layout
    Imagens e cores adicionadas e descrições alteradas

--- POSEIDON/pages/Equipe.py
+++ POSEIDON/pages/Equipe.py
@@ -1,15 +1,55 @@
 import streamlit as sl
-from PIL import Image
 
 sl.title('Conheça nossa equipe')
-sl.subheader('Gabriel Sakura')
-sl.text("Nosso Scrum Master, e responsável por toda a programação dos sensores")
-sl.subheader('Gabriel Trusnovec')
-sl.text("Responsável por toda a parte de CAD, que resultou numa parte fundamental da maquete")
-sl.subheader('Matheus')
-sl.text("Ajudou em quase todas as partes do projeto como o site, a montagem eletrônica e na")
-sl.text("maquete")
-sl.subheader('Gabriel Tadashi')
-sl.text("Foi o Product Backowner(PO) de todo projeto, e ficou responsável pelo site")
-sl.subheader('Giulia Henritzi')
-sl.text("Foi uma peça chave para a realização da maquete, além de ser a apresentadora")
+
+sl.markdown(f'''
+    <div class="card">
+        <h2>Gabriel Tadashi</h2>
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/auyPEqH.jpg" alt="Gabriel Sakura">
+        <div class="card-body">
+            <h4 class="card-title" style="color: #c95b00">PO</h4>
+            <h6>Principal responsável pela parte organizacional do projeto e pelo site</h6>
+        </div>
+    </div>
+''', unsafe_allow_html=True)
+
+sl.markdown(f'''
+    <div class="card">
+        <h2>Gabriel Sakura</h2>
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/4lJxXwY.jpg" alt="Gabriel Sakura">
+        <div class="card-body">
+            <h4 class="card-title" style="color: #27963c">Scrum Master</h4>
+            <h6>O responsável por grande parte da programação dos sensores</h6>
+        </div>
+    </div>
+''', unsafe_allow_html=True)
+
+sl.markdown(f'''<div class="card">
+        <h2>Matheus Silva</h2>
+        </div>
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/YqFw6VS.jpg" alt="Gabriel Sakura">
+        <h4 class="card-title" style="color: #00c4bb">Desenvolvedor Geral</h4>
+        <h6>Participou em quase todas as etapas do projeto, incluindo o site, a montagem eletrônica, da na maquete e idealização</h6>
+''', unsafe_allow_html=True)
+
+sl.markdown(f'''
+    <div class="card">
+        <h2>Gabriel Trusnovec</h2>
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/zfgP340.jpg" alt="Gabriel Sakura">
+        <div class="card-body">
+            <h4 class="card-title" style="color: #1a10cc">Desenvolvedor CAD</h4>
+            <h6>Responsável por toda a parte de CAD, parte fundamental da maquete</h6>
+        </div>
+    </div>
+''', unsafe_allow_html=True)
+
+sl.markdown(f'''
+    <div class="card">
+        <h2>Giulia Henritzi</h2>
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/3NLdXWI.jpg" alt="Gabriel Sakura">
+        <div class="card-body">
+            <h4 class="card-title" style="color: #8500ad">Desenvolvedora Porta-Voz</h4>
+            <h6 class="card-title">Desempenhou papel importante para a realização da maquete, além de ser a porta-voz do projeto</h6>
+        </div>
+    </div>
+''', unsafe_allow_html=True)
