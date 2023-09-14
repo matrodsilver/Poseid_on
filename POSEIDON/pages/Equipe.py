61ca2cb6012d5840367e33ff77ab61802fce9ad7
MR.S <82432761+matrodsilver@users.noreply.github.com>
2023-09-14 20:14:12 -0300

    Update Equipe.py

--- POSEIDON/pages/Equipe.py
+++ POSEIDON/pages/Equipe.py
@@ -5,7 +5,7 @@ sl.title('Conheça nossa equipe')
 sl.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">', unsafe_allow_html=True)
 
 sl.markdown(f'''
-    <div class="card">
+    <div class="card" style="color: #3781b0;">
         <h2>Gabriel Tadashi</h2>
         <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/auyPEqH.jpg" alt="Gabriel Sakura">
         <div class="card-body">
