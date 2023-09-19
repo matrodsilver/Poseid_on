e2321f90e2644998c1c94a03b121b4b06b6f23d0
MR.S <82432761+matrodsilver@users.noreply.github.com>
2023-09-19 13:09:10 -0300

    comentado

--- notificar.py
+++ notificar.py
@@ -1,42 +1,50 @@
-from pushbullet import Pushbullet
-import requests
-import time
-import pandas as p
+''' Código para envio de dados do clima e notificação sobre o volume da caixa '''
 
 
+from pushbullet import Pushbullet # biblioteca para comunicação com o app de notificações
+import requests # biblioteca para comunicação com links
+import time # biblioteca que cont o tempo
+import pandas as p # biblioteca para organização de dados
+
+
+# função que pega os valores do clima atual e manda para a nuvem
 def pegarValores():
   urlTSultimoResultado = f'https://api.thingspeak.com/channels/2127654/feeds.json?api_key=MZB0IDFGQR9AQVBW&results=1'
 
   resposta = requests.get(urlTSultimoResultado)
 
-  if resposta.status_code == 200:
-    return resposta.json()
+  if resposta.status_code == 200: # se conexão funcionou
+    
+    return resposta.json() # retornar arquivo json com os dados do clima
+    
   else:
     print('Erro na requisição')
     return {}
 
 
+# função responsável por verificar volume da caixa retentora e notificar se necessário
 def avisar():
-  if int(pegarValores()['feeds'][0]['field1']) > 1:
-    print('sin')
-    
+  if int(pegarValores()['feeds'][0]['field1']) > 1: # se volume passou do limite
+    print('Volume excedeu limite')
+
+    # usuários a serem notificados
     eu = 'o.9CYuBlpove3ErChfkLDjcmkNcjquJ1oz'
     tadashi = 'x'
 
-    usuarios = [eu]
+    usuarios = [eu] # lista a ser percorrida com otodos os usuários
     
-    for usuario in usuarios:  # type:ignore
+    for usuario in usuarios: 
       pbt = Pushbullet(usuario)
-      pbt.push_note(
-          '⚠️Aviso⚠️', '⚠ O bueiro【𝟭】de São Paulo atingiu o limite de volume ⚠')
+      pbt.push_note('⚠️Aviso⚠️', '⚠ O bueiro【𝟭】de São Paulo atingiu o limite de volume ⚠') # notificação
 
 
+# função responsável por enviar informações do clima atual para o servidor
 def enviar():
-  token = '4127401294a510735af86031ebc9697b'
+  token = '4127401294a510735af86031ebc9697b' # chave da conta usada para requisição
+  
   # FIAP(lat, long) = -23.57323583564156, -46.623008521519246
 
   urlCurrent = f'https://api.openweathermap.org/data/2.5/weather?lat={-23.57323583564156}&lon={-46.623008521519246}&appid={token}&units=metric&lang={"pt_br"}'
-  print(urlCurrent)
   reqCurrent = requests.get(urlCurrent)
   infoCurrent = reqCurrent.json()
 
@@ -50,13 +58,13 @@ def enviar():
   icone = infoCurrent['weather'][0]['icon']
 
   requests.post(f"https://api.thingspeak.com/update?api_key=MB4W5VR14OISKTZU&units=metric&lang={'pt_br'}&field1='{descricaoCurrent}'&field2='{mainCurrent}'&field3={chuva1h}&field4={umidadeCurrent}&field5={ventoVelocidadeCurrent}&field6={ventoAnguloCurrent}")#&field7={None}&field8={None}")
-  print(infoCurrent)
+  print('enviado')
 
 
 enviar()
 
 while True:
-  #avisar()
+  #avisar() # debug: desativado por hora porque valor limite é muito baixo e fica notificando sempre
   enviar()
 
   time.sleep(20)
