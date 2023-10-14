a2f1100f2d8a718b6a324e7b56d9b7b1a982f9dd
MR.S <82432761+matrodsilver@users.noreply.github.com>
2023-10-13 22:18:45 -0300

    Update Funfa.cpp

--- Funfa.cpp
+++ Funfa.cpp
@@ -5,11 +5,20 @@
 // variáveis referentes ao sensor de ultrassom
 const byte triggerPin = 26;
 const byte echoPin = 25;
+
+const byte triggerPin2 = 21;
+const byte echoPin2 = 19;
+
 const int maxCm = 404;
 
 UltraSonicDistanceSensor sensorUltrassom(triggerPin, echoPin, maxCm);
+UltraSonicDistanceSensor sensorUltrassom2(triggerPin2, echoPin2, maxCm);
 
 float distancia;
+float distancia2;
+
+float mudarDistancia;
+float mudarDistancia2;
 
 // variáveis referentes ao sensor infravermelho
 const byte sensorInfra = 33;
@@ -18,10 +27,15 @@ const byte sensorInfra2 = 32;
 bool infra;
 bool infra2;
 
+bool mudarInfra;
+bool mudarInfra2;
+
 // variáveis referentes ao sensor de nível de água
 const byte sensorAgua = 35;
 
 float agua;
+
+float mudarAgua;
 
 // variáveis referentes ao led
 const byte r = 15;
@@ -78,22 +92,55 @@ void loop()
   if (tempo % 500 == 0)
   {
     distancia = sensorUltrassom.measureDistanceCm();
+    distancia2 = sensorUltrassom2.measureDistanceCm();
 
-    Serial.println("Distância-pré manipulação: " + String(distancia));
+    if (distancia != mudarDistancia)
+    {
+      Serial.println("Distância-pré manipulação: " + String(distancia));
+
+      mudarDistancia = distancia;
+    }
+    if (distancia2 != mudarDistancia2 && distancia2 > 19)
+    {
+      Serial.println("Distância2-pré manipulação: " + String(distancia2));
+
+      mudarDistancia2 = distancia2;
+    }
 
     if (distancia < 19)
     {
       distancia = 19;
     }
-    else if (distancia > 80)
+    else if (distancia > 57)
     {
-      distancia = 80;
+      distancia = 57;
     }
 
-    Serial.println("Distância-pós manipulação: " + String(distancia));
+    if (distancia2 < 19)
+    {
+      distancia2 = 19;
+    }
+    else if (distancia2 > 57)
+    {
+      distancia2 = 57;
+    }
 
-    analogWrite(r, 255 - map(distancia, 19, 80, 0, 255));
-    analogWrite(g, map(distancia, 19, 80, 0, 255));
+    if (distancia != mudarDistancia)
+    {
+      Serial.println("Distância-pós manipulação: " + String(distancia));
+
+      mudarDistancia = distancia;
+    }
+
+    if (distancia2 != mudarDistancia2 && distancia2 > 19)
+    {
+      Serial.println("Distância2-pós manipulação: " + String(distancia2));
+
+      mudarDistancia2 = distancia2;
+    }
+
+    analogWrite(r, 255 - map(distancia, 19, 57, 0, 255));
+    analogWrite(g, map(distancia, 19, 57, 0, 255));
 
     if (tempo % 20000 == 0)
     {
@@ -104,11 +151,25 @@ void loop()
         infra = !digitalRead(sensorInfra);
         infra2 = !digitalRead(sensorInfra2);
         agua = analogRead(sensorAgua);
-        
 
-        Serial.println("Infra1: " + String(infra));
-        Serial.println("Infra2: " + String(infra2));
-        Serial.println("Nível de Água: " + String(agua));
+        if (infra != mudarInfra)
+        {
+          Serial.println("Infra: " + String(infra));
+
+          mudarInfra = infra;
+        }
+        if (infra2 != mudarInfra2)
+        {
+          Serial.println("Infra2: " + String(infra2));
+
+          mudarInfra2 = infra2;
+        }
+        if (agua != mudarAgua)
+        {
+          Serial.println("Nível de Água: " + String(agua));
+
+          mudarAgua = agua;
+        }
 
         request_string = thingSpeakAddress;
         request_string += "&field1=";
@@ -119,6 +180,15 @@ void loop()
         request_string += infra;
         request_string += "&field4=";
         request_string += infra2;
+        request_string += "&field5=";
+        if (distancia2 > 19)
+        {
+          request_string += distancia2;
+        }
+        else
+        {
+          request_string += 0;
+        }
 
         client.print("POST /update HTTP/1.1\n");
         client.print("Host: api.thingspeak.com\n");
