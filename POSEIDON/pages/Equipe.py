ba38d34635c1e171c673ec46b1b109bba75ea61d
MR.S <82432761+matrodsilver@users.noreply.github.com>
2023-10-13 22:20:31 -0300

    update links

--- POSEIDON/pages/Equipe.py
+++ POSEIDON/pages/Equipe.py
@@ -1,10 +1,10 @@
 import streamlit as sl
 
-sl.title('Conheça nossa equipe')
-
 sl.markdown(f'''
+            <h1 style="height: 5rem;">Conheça nossa equipe</h1>
+            <h6 style="color: #7d7d7d; height: 3rem;">Links para informações de cada membro em seus respectivos nomes</h6>
     <div class="card">
-        <h2>Gabriel Tadashi</h2>
+        <h2><a style="color: #c95b00" href="https://www.linkedin.com/in/gtadashi/">Gabriel Tadashi</a></h2>
         <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/auyPEqH.jpg" alt="Gabriel Sakura">
         <div class="card-body">
             <h4 class="card-title" style="color: #c95b00">PO</h4>
@@ -15,7 +15,7 @@ sl.markdown(f'''
 
 sl.markdown(f'''
     <div class="card">
-        <h2>Gabriel Sakura</h2>
+        <h2><a style="color: #27963c" href="https://www.linkedin.com/in/gabriel-alexandre-sakura-1a542528b/">Gabriel Sakura</a></h2>
         <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/4lJxXwY.jpg" alt="Gabriel Sakura">
         <div class="card-body">
             <h4 class="card-title" style="color: #27963c">Scrum Master</h4>
@@ -25,7 +25,7 @@ sl.markdown(f'''
 ''', unsafe_allow_html=True)
 
 sl.markdown(f'''<div class="card">
-        <h2>Matheus Silva</h2>
+        <h2><a style="color: #00c4bb" href="https://www.linkedin.com/in/matheus-rodrigues-da-silva-30b568267/">Matheus Silva</a></h2>
         </div>
         <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/YqFw6VS.jpg" alt="Gabriel Sakura">
         <h4 class="card-title" style="color: #00c4bb">Desenvolvedor Geral</h4>
@@ -34,7 +34,7 @@ sl.markdown(f'''<div class="card">
 
 sl.markdown(f'''
     <div class="card">
-        <h2>Gabriel Trusnovec</h2>
+        <h2><a style="color: #1a10cc" href="https://www.linkedin.com/in/gabriel-trusnovec-mateus-a079a7267/">Gabriel Trusnovec</a></h2>
         <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/zfgP340.jpg" alt="Gabriel Sakura">
         <div class="card-body">
             <h4 class="card-title" style="color: #1a10cc">Desenvolvedor CAD</h4>
@@ -45,7 +45,7 @@ sl.markdown(f'''
 
 sl.markdown(f'''
     <div class="card">
-        <h2>Giulia Henritzi</h2>
+        <h2><a style="color: #8500ad" href="https://www.linkedin.com/in/giulia-henritzi-6375b0288/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Giulia Henritzi</a></h2>
         <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/3NLdXWI.jpg" alt="Gabriel Sakura">
         <div class="card-body">
             <h4 class="card-title" style="color: #8500ad">Desenvolvedora Porta-Voz</h4>
