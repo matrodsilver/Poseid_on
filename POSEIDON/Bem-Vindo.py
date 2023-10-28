5fa4a1e141997f9e936b262269b87518338930c4
MR.S <82432761+matrodsilver@users.noreply.github.com>
2023-10-27 23:57:50 -0300

    v1.0

--- POSEIDON/Bem-Vindo.py
+++ POSEIDON/Bem-Vindo.py
@@ -1,3 +1,5 @@
+# Página estilizada usando HTML e a biblioteca streamlit para abranger o escopo do projeto
+
 import streamlit as sl
 
 sl.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">', unsafe_allow_html=True)
@@ -11,33 +13,33 @@ sl.markdown('''
     </div>        
 
     <p>
-    <h3 style="text-align: center;">Sobre o projeto <span style="color:#3b8dbf"> P.O.S.E.I.D.</span><span style="color: #50c878">ON</h3>
+    <h3 style="text-align: center;">Sobre o projeto <span style="color:#3b8dbf"> P.O.S.E.I.D.</span><span style="color: #50c878">ON</span></h3>
                 
-    Hoje, trago a vocês uma visão de mudança para as cidades em que vivemos. Comecemos refletindo sobre algo que muitas vezes passa despercebido: os bueiros entupidos. Esses vilões urbanos podem causar estragos significativos, desde enchentes até doenças e caos nas ruas.
+    Hoje, trago a vocês uma visão de **mudança** para as cidades em que vivemos. Comecemos refletindo sobre algo que muitas vezes passa despercebido: os bueiros entupidos. Esses vilões urbanos podem causar estragos significativos, desde enchentes até doenças e caos nas ruas.
 
     </p> 
     <p>        
-    <h3 style="text-align: center;">Desafios dos Bueiros Entupidos</h3>
+    <h3 style="text-align: center;"><span style="color:#ff2400">Desafios </span> dos Bueiros Entupidos</h3>
                 
     Já se perguntaram sobre os problemas causados por bueiros entupidos? O cenário é sombrio. As chuvas transformam as ruas em rios, causando prejuízos materiais e ameaçando vidas. A água contaminada traz consigo doenças que se espalham rapidamente, afetando a saúde pública. Além disso, o lixo acumulado atrai pragas urbanas, prejudicando a qualidade de vida.
     </p>
     <p>
-    <h3 style="text-align: center;">Vislumbrando a Mudança</h3>
+    <h3 style="text-align: center;">Vislumbrando a <span style="color:#00FF00">Mudança</span></h3>
                 
     Agora, imagine um futuro em que as enchentes sejam minimizadas, doenças prevenidas e nossas cidades mais limpas e saudáveis. Hoje, apresento a vocês o projeto P.O.S.E.I.D.ON. Nomeado após o deus grego dos mares, este projeto visa trazer um novo olhar para o gerenciamento de bueiros.
     </p>
     <p>
-    <h3 style="text-align: center;">O Projeto P.O.S.E.I.D.ON</h3>
+    <h3 style="text-align: center;">O Projeto <span style="color:#3b8dbf"> P.O.S.E.I.D.</span><span style="color: #50c878">ON</span></h3>
                 
     "Possuído" por uma ideia ousada, o projeto P.O.S.E.I.D.ON traz soluções tecnológicas de ponta para enfrentar um desafio antigo. Transformaremos os bueiros em ativos inteligentes.
     </p>
     <p>
-    <h3 style="text-align: center;">Impacto e Futuro</h3>
+    <h3 style="text-align: center;"><span style="color:#00703e">Impacto</span> e <span style="color:#00703e">Futuro</span></h3>
                 
-    O impacto é notável. Menos enchentes significam cidades mais seguras, economia de recursos e vidas protegidas. Com cidades mais limpas e saudáveis, a qualidade de vida aumenta para todos nós. E o melhor? Este é apenas o começo. Podemos expandir essa solução para outras cidades, em todo o mundo.
+    O impacto é notável. Menos enchentes significam cidades mais **seguras, economia de recursos e vidas protegidas.** Com cidades mais limpas e saudáveis, a qualidade de vida aumenta para todos nós. E o melhor? Este é apenas o começo. Podemos expandir essa solução para outras cidades, em todo o mundo.
     </p>
     <p>        
-    <h3 style="text-align: center;">Conclusão</h3>
+    <h3 style="text-align: center; color: #4169e1">Conclusão</h3>
                 
     O projeto P.O.S.E.I.D.ON não é apenas uma ideia... é o começo de uma revolução. Ao combinar tecnologia, inovação e colaboração, estamos redefinindo a maneira como nossas cidades enfrentam os desafios das águas das chuvas. Muito obrigado por nos acompanhar hoje. Juntos, estamos construindo cidades mais seguras, limpas e conectadas.
     </p>
