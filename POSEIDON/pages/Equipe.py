497298e9f411fd846e5a7e8d91df66283629d9df
MR.S <82432761+matrodsilver@users.noreply.github.com>
2024-03-08 17:58:43 -0300

    V3.0

--- POSEIDON/pages/Equipe.py
+++ POSEIDON/pages/Equipe.py
@@ -7,7 +7,7 @@ sl.markdown(f'''
             <h6 style="color: #7d7d7d; height: 3rem;">Links para informações de cada membro em seus respectivos nomes</h6>
     <div class="card">
         <h2><a style="color: #c95b00" href="https://www.linkedin.com/in/gtadashi/">Gabriel Tadashi</a></h2>
-        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/auyPEqH.jpg" alt="Gabriel Sakura">
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/auyPEqH.jpg" alt="Gabriel Tadashi">
         <div class="card-body">
             <h4 class="card-title" style="color: #c95b00">PO</h4>
             <h6>Principal responsável pela parte organizacional do projeto e pelo site</h6>
@@ -29,7 +29,7 @@ sl.markdown(f'''
 sl.markdown(f'''<div class="card">
         <h2><a style="color: #00c4bb" href="https://www.linkedin.com/in/matheus-rodrigues-da-silva-30b568267/">Matheus Silva</a></h2>
         </div>
-        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/fgBIVIO.jpg" alt="Gabriel Sakura">
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/UAQyE3b.jpg" alt="Matheus Rodrigues da Silva">
         <h4 class="card-title" style="color: #00c4bb">Desenvolvedor Geral</h4>
         <h6>Participou em quase todas as etapas do projeto, incluindo o site, a montagem eletrônica, confecção da maquete e idealização</h6>
 ''', unsafe_allow_html=True)
@@ -37,7 +37,7 @@ sl.markdown(f'''<div class="card">
 sl.markdown(f'''
     <div class="card">
         <h2><a style="color: #1a10cc" href="https://www.linkedin.com/in/gabriel-trusnovec-mateus-a079a7267/">Gabriel Trusnovec</a></h2>
-        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/zfgP340.jpg" alt="Gabriel Sakura">
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/zfgP340.jpg" alt="Gabriel Trusnovec">
         <div class="card-body">
             <h4 class="card-title" style="color: #1a10cc">Desenvolvedor CAD</h4>
             <h6>Responsável por toda a parte de CAD, parte fundamental da maquete</h6>
@@ -48,7 +48,7 @@ sl.markdown(f'''
 sl.markdown(f'''
     <div class="card">
         <h2><a style="color: #8500ad" href="https://www.linkedin.com/in/giulia-henritzi-6375b0288/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Giulia Henritzi</a></h2>
-        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/3NLdXWI.jpg" alt="Gabriel Sakura">
+        <img class="card-img-top" style="width: 10rem;" src="https://i.imgur.com/3NLdXWI.jpg" alt="Giulia Henritzi">
         <div class="card-body">
             <h4 class="card-title" style="color: #8500ad">Desenvolvedora Porta-Voz</h4>
             <h6 class="card-title">Desempenhou papel importante para a realização da maquete, além de ser a porta-voz do projeto</h6>
